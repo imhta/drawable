986323eea2f8975d9e68777bf949843e7148449b
imhta <imhtapm@gmail.com>
2019-09-07 23:04:01 +0530

    removing default prop in stencil

--- src/components.d.ts
+++ src/components.d.ts
@@ -9,20 +9,7 @@ import { HTMLStencilElement, JSXBase } from '@stencil/core/internal';
 
 
 export namespace Components {
-  interface MyComponent {
-    /**
-    * The first name
-    */
-    'first': string;
-    /**
-    * The last name
-    */
-    'last': string;
-    /**
-    * The middle name
-    */
-    'middle': string;
-  }
+  interface MyComponent {}
 }
 
 declare global {
@@ -39,20 +26,7 @@ declare global {
 }
 
 declare namespace LocalJSX {
-  interface MyComponent extends JSXBase.HTMLAttributes<HTMLMyComponentElement> {
-    /**
-    * The first name
-    */
-    'first'?: string;
-    /**
-    * The last name
-    */
-    'last'?: string;
-    /**
-    * The middle name
-    */
-    'middle'?: string;
-  }
+  interface MyComponent extends JSXBase.HTMLAttributes<HTMLMyComponentElement> {}
 
   interface IntrinsicElements {
     'my-component': MyComponent;
--- src/components/my-component/my-component.tsx
+++ src/components/my-component/my-component.tsx
@@ -1,5 +1,4 @@
 import { Component, Prop, h } from '@stencil/core';
-import { format } from '../../utils/utils';
 
 @Component({
   tag: 'my-component',
@@ -10,23 +9,23 @@ export class MyComponent {
   /**
    * The first name
    */
-  @Prop() first: string;
+  // @Prop() first: string;
 
   /**
    * The middle name
    */
-  @Prop() middle: string;
+  // @Prop() middle: string;
 
   /**
    * The last name
    */
-  @Prop() last: string;
+  // @Prop() last: string;
 
-  private getText(): string {
-    return format(this.first, this.middle, this.last);
-  }
+  // private getText(): string {
+  //   return format(this.first, this.middle, this.last);
+  // }
 
   render() {
-    return <div>Hello, World! I'm {this.getText()}</div>;
+    return <div>Hello, World!</div>;
   }
 }
